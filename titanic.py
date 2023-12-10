7f7a9c5571e5fc2ffbdf3d3fd4ab6343f4141f7a
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-12-10 18:05:22 +0100

    Update titanic.py

--- titanic.py
+++ titanic.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-import pandas
+import pandas as pd
+# import logistic regression
+data = pd.read_csv('train.csv')
+print(data.head())
+
+
+if __name__ == '__main__':
+  print('logistic regression')
