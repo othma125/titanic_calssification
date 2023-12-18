1ecc02035fa42e6f27b08a2ce011f9f9a99c9448
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-12-18 14:49:08 +0100

    logistic_regression_result.csv

--- titanic.py
+++ titanic.py
@@ -1,56 +1,55 @@
 #!/usr/bin/python3
-""" Logistic Regression for titanic survival prediction
+""" machine learning for titanic survival prediction
 """
 import pandas as pd
-# import logistic regression
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
 train_data = pd.read_csv('train.csv')
+# print(train_data.isnull().sum())
+train_data['Age']=train_data['Age'].fillna(train_data['Age'].mean())
+# print(train_data.isnull().sum())
+
 test_data = pd.read_csv('test.csv')
+# print(test_data.isnull().sum())
+test_data['Age']=test_data['Age'].fillna(test_data['Age'].mean())
+test_data['Fare']=test_data['Fare'].fillna(test_data['Fare'].mean())
+# print(test_data.isnull().sum())
+# Encoding the 'Sex' column (male: 1, female: 0)
+label_encoder = LabelEncoder()
+train_data['Sex'] = label_encoder.fit_transform(train_data['Sex'])
+test_data['Sex'] = label_encoder.fit_transform(test_data['Sex'])
 
 
 def logistic_regression():
   """ Logistic Regression for titanic survival prediction
   """
+  print('logistic regression')
   # Preprocessing
-  # Only using the 'Sex' feature for prediction
-  # Encoding the 'Sex' column (male: 1, female: 0)
-  label_encoder = LabelEncoder()
-  print(train_data['Sex'].head())
-  train_data['Sex'] = label_encoder.fit_transform(train_data['Sex'])
-  print(train_data['Sex'].head())
-
+  # Only using the 'Sex', "Age" ,"Pclass" and "Fare" feature for prediction
   # Selecting features and target
-  X = train_data[['Sex']]  # Features
+  X = train_data[["Sex", "Age" ,"Pclass","Fare"]]  # Features
   Y = train_data['Survived']  # Target
 
   # Splitting the data into training and testing sets
-  X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+  x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
 
-  # Logistic Regression
-  log_reg = LogisticRegression()
-  log_reg.fit(X_train, y_train)
-
-  # Predictions
-  new_predict = log_reg.predict(label_encoder.fit_transform(test_data['Sex']))
+  # Logistic Regression Training
+  model = LogisticRegression()
+  model.fit(x_train, y_train)
 
   # Calculating the accuracy
-  # accuracy = accuracy_score(y_test, y_pred)
-  # print(accuracy)
+
+  accuracy = accuracy_score(model.predict(x_test), y_test)
+  print(f'\tlogistic regression accuracy = {accuracy}')
+
   #creating submission file
-  
-  label_encoder = LabelEncoder()
-  test_data['Sex'] = label_encoder.fit_transform(test_data['Sex'])
+  new_predict = model.predict(test_data[["Sex", "Age" ,"Pclass","Fare"]])
   result = pd.DataFrame({'PassengerId': test_data['PassengerId'],
-                          'Sex': test_data['Sex'],
                           'Survived': new_predict})
-  result.to_csv('submission.csv', index=False)
+  result.to_csv('logistic_regression_result.csv', index=False)
 
 
 if __name__ == '__main__':
-  print('logistic regression')
-  # print(train_data.head())
-  # print(test_data.head())
   logistic_regression()
